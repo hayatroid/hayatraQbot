97f7201f1986ec456839afbfd2c7301654137bb0
hayatroid <hayatroid@gmail.com>
2025-12-09 09:30:48 +0000

    mod_add, sub, mul, div

--- src/cli.rs
+++ src/cli.rs
@@ -10,17 +10,29 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Retruns the multiplicative inverse of `self`.
-    Inv { val: u32 },
-    /// Returns `self` to the power of `n`.
-    Pow { val: u32, n: u64 },
+    /// Returns the multiplicative inverse of `val`.
+    Inv { val: u64 },
+    /// Returns `lhs` plus `rhs`.
+    Add { lhs: u64, rhs: u64 },
+    /// Returns `lhs` minus `rhs`.
+    Sub { lhs: u64, rhs: u64 },
+    /// Returns `lhs` multiplied by `rhs`.
+    Mul { lhs: u64, rhs: u64 },
+    /// Returns `lhs` divided by `rhs`.
+    Div { lhs: u64, rhs: u64 },
+    /// Returns `base` to the power of `exp`.
+    Pow { base: u64, exp: u64 },
 }
 
 impl Cli {
     pub fn run(&self) -> String {
         match self.command {
             Commands::Inv { val } => ModInt998244353::new(val).inv().to_string(),
-            Commands::Pow { val, n } => ModInt998244353::new(val).pow(n).to_string(),
+            Commands::Add { lhs, rhs } => (ModInt998244353::new(lhs) + rhs).to_string(),
+            Commands::Sub { lhs, rhs } => (ModInt998244353::new(lhs) - rhs).to_string(),
+            Commands::Mul { lhs, rhs } => (ModInt998244353::new(lhs) * rhs).to_string(),
+            Commands::Div { lhs, rhs } => (ModInt998244353::new(lhs) / rhs).to_string(),
+            Commands::Pow { base, exp } => ModInt998244353::new(base).pow(exp).to_string(),
         }
     }
 }
